afbd74aebc67749a0898518b446b0b830d2f0de9
PurifiedPotion <albatrers@gmail.com>
2025-07-15 21:01:17 +0900

    table scheme updated

--- insertHP.js
+++ insertHP.js
@@ -39,8 +39,10 @@ const query = `
     page_views,
     page_exits,
     drop_rate,
-    avg_time_on_page_seconds,
-    sdk_key
+    next_pages_to,
+    next_pages_count,
+    sdk_key,
+    avg_time_on_page_seconds
   FROM (
     SELECT
       toStartOfHour(date_time) AS grouped_time,
@@ -49,6 +51,8 @@ const query = `
       sum(page_views) AS page_views,
       sum(page_exits) AS page_exits,
       avg(drop_rate) AS drop_rate,
+      groupArray(\`next_pages.to\`) AS next_pages_to,
+      groupArray(\`next_pages.count\`) AS next_pages_count,
       avg(avg_time_on_page_seconds) AS avg_time_on_page_seconds
     FROM klicklab.minutes_page_stats
     WHERE date_time >= toDateTime('${start.format("YYYY-MM-DD HH:mm:ss")}')
